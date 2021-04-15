f74f94390941bfeeb03102ade6d533ff7cae5980
lievan <0evanli0@gmail.com>
2021-04-15 00:58:38 -0400

    fixed inconsistent styling

--- config.js
+++ config.js
@@ -19,7 +19,7 @@ var config = {
             id: 'iliad',
             alignment: 'left',
             hidden: false,
-            title: '<span class="underline--magical">Iliad</span> by <span class="underline--magical">Homer',
+            title: '<span class="underline--magical">Iliad</span> by Homer',
             date: '<span class="date">762 B.C.</span>',
             image: 'images/homer.png',
             description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
